66529b5563926b32e600af06cdc4d8a377bdf6ee
Joe Pierce <joepierce88@gmail.com>
2025-06-01 13:12:31 -0500

    fix docx file

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,4 +1,4 @@
 from .database import init_db, get_db_connection, get_user_clients, get_templates_for_client, get_conversion_prompts_for_client
 from .document import process_docx, process_text_input
 from .conversion import convert_content
-from .docx_builder import create_reformatted_docx, apply_docx_style
+from .docx_builder import create_reformatted_docx
